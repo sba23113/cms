9950b570e4424949864ad7a1b3ef7176aae8778d
LHomola <sba23113@student.cct.ie>
2024-03-29 23:01:55 +0000

    add and implement getUser(String username) method

--- src/dao/UserDaoImpl.java
+++ src/dao/UserDaoImpl.java
@@ -5,10 +5,39 @@
 
 package dao;
 
+import tables.User;
+import java.sql.*;
+
 /**
  * 
  * @author Lukas Homola <sba23113@student.cct.ie>
  */
-public class UserDaoImpl {
+public class UserDaoImpl implements UserDao {
+    private Connection conn;
+    
+    public UserDaoImpl(Connection conn) {
+        this.conn = conn;
+    }
 
+    @Override
+    public User getUser(String username) {
+        User user = null;
+        String sql = "SELECT * FROM Users WHERE Username = ?";
+        
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        pstmt.setString(1, username);
+        ResultSet rs = pstmt.executeQuery();
+
+        if (rs.next()) {
+            int userID = rs.getInt("UserID");
+            String userRole = rs.getString("UserRole");
+            String passwordHash = rs.getString("PasswordHash");
+            user = new User(userID, username, passwordHash, userRole);
+        }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        
+        return user;
+    }
 }
