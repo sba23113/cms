8eaa6c92ff33a8a31df0e81dcf24d73cbec7eb69
LHomola <sba23113@student.cct.ie>
2024-03-29 18:52:34 +0000

    Update Course.java with fields, getters and setters
       private int courseID;
        private String courseName;

--- src/Tables/Course.java
+++ src/Tables/Course.java
@@ -10,5 +10,29 @@ package Tables;
  * @author Lukas Homola <sba23113@student.cct.ie>>
  */
 public class Course {
-    
+    private int courseID;
+    private String courseName;
+
+    // Constructor
+    public Course(int courseID, String courseName) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+    }
+
+    // Getters and Setters
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
 }
