34200c11032bba0347c1bb9fe5e609f288cbf410
LHomola <sba23113@student.cct.ie>
2024-03-29 21:55:29 +0000

    delete UserDao.java class and create UserDao.java interface

--- src/Dao/UserDao.java
+++ src/Dao/UserDao.java
@@ -1,15 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-
 package Dao;
 
-import Tables.User;
 /**
- * 
+ *
  * @author Lukas Homola <sba23113@student.cct.ie>
  */
-public class UserDao {
+public interface UserDao {
     
 }
