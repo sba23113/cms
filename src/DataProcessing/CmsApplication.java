ee8b4fc43aab798ef78da6b54ee9dcba0af771d3
LHomola <sba23113@student.cct.ie>
2024-03-29 16:03:31 +0000

    Initial commit

---
+++ src/DataProcessing/CmsApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package DataProcessing;
+
+/**
+ *
+ * @author Lukas Homola sba23113@student.cct.ie
+ */
+public class CmsApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
