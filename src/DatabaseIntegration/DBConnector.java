175d4dab04cdd269cf68c08c1ee6fb9df0c689c9
LHomola <sba23113@student.cct.ie>
2024-03-29 16:05:12 +0000

    Create DBConnector.java
    Inside new package: DatabaseIntegration

---
+++ src/DatabaseIntegration/DBConnector.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package DatabaseIntegration;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class DBConnector {
+
+}
