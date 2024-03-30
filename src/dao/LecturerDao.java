0291e479d039bd9b3ac6cc804036ac14a2ba7298
LHomola <sba23113@student.cct.ie>
2024-03-30 19:23:02 +0000

    create LecturerDao and StudentDao interfaces
    missing relevant source classes (tables.Student and tables.Lecturer) -> to be implemented next

---
+++ src/dao/LecturerDao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import tables.Lecturer;
+import java.util.List;
+
+/**
+ *
+ * @author Lukas Homola <sba23113@student.cct.ie>
+ */
+public interface LecturerDao {
+    Lecturer getLecturerById(int lecturerId);
+    List<Lecturer> getAllLecturers();
+}
