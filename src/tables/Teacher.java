b1043b6645bd6a31a3843fe3ce33b73c0447ec86
LHomola <sba23113@student.cct.ie>
2024-03-30 20:20:45 +0000

    Update Teacher.java

--- src/tables/Teacher.java
+++ src/tables/Teacher.java
@@ -10,5 +10,109 @@ package tables;
  * @author Lukas Homola <sba23113@student.cct.ie>
  */
 public class Teacher {
+    private int lecturerID;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String phoneNumber;
+        private int userID;
+        private String lecturerRole;
+        private int birthYear;
+        private int birthMonth;
+        private int birthDay;
 
+    public Teacher(int lecturerID, String firstName, String lastName, String email, String phoneNumber, int userID, String lecturerRole, int birthYear, int birthMonth, int birthDay) {
+        this.lecturerID = lecturerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.userID = userID;
+        this.lecturerRole = lecturerRole;
+        this.birthYear = birthYear;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+    }
+
+    public int getLecturerID() {
+        return lecturerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getLecturerRole() {
+        return lecturerRole;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public int getBirthMonth() {
+        return birthMonth;
+    }
+
+    public int getBirthDay() {
+        return birthDay;
+    }
+
+    public void setLecturerID(int lecturerID) {
+        this.lecturerID = lecturerID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setLecturerRole(String lecturerRole) {
+        this.lecturerRole = lecturerRole;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setBirthMonth(int birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public void setBirthDay(int birthDay) {
+        this.birthDay = birthDay;
+    }
+        
+        
 }
