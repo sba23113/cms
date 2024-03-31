0d8c43b89fcd54a1f53474cc7042658761f66951
LHomola <sba23113@student.cct.ie>
2024-03-31 00:46:30 +0000

    Create GradeDaoImpl.java
    public class GradeDaoImpl implements GradeDao

---
+++ src/dao/GradeDaoImpl.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package dao;
+
+import tables.Grade;
+import java.sql.*;
+
+/**
+ * 
+ * @author Lukas Homola <sba23113@student.cct.ie>
+ */
+public class GradeDaoImpl implements GradeDao {
+    private Connection conn;
+
+    public GradeDaoImpl(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public Grade getGradeById(int gradeId) {
+        String sql = "SELECT * FROM Grade WHERE GradeID = ?"; // SQL query to select all columns from the Grades table where the GradeID column matches the requested value
+        Grade grade = null; // Initialize Grade object to null so that null is returned if matching Grade is not found
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) { // PreparedStatement to prevent SQL injection attacks wrapped in try-with-resources to prevent resouce leaks
+            pstmt.setInt(1, gradeId); // Replace the ? in the above SQL query with the provided studentId
+            try (ResultSet rs = pstmt.executeQuery()) { // Execute the SQL query -> store result in ResultSet object
+                if (rs.next()) {  // Check if the query returned at least 1 result
+                    grade = new Grade(  // Create a new Grade object
+                        rs.getInt("GradeID"),
+                        rs.getInt("EnrollmentID"),
+                        rs.getFloat("GradeValue")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return grade;  //  Return Grade object
+    }
+}
